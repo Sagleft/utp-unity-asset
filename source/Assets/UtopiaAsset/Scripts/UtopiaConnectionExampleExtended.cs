d1d56af834f787fe6a277ba2c70c421cd51ea8aa
Sagleft <sagleft@inbox.ru>
2020-09-13 16:44:09 +0400

    added user data loading

--- source/Assets/UtopiaAsset/Scripts/UtopiaConnectionExampleExtended.cs
+++ source/Assets/UtopiaAsset/Scripts/UtopiaConnectionExampleExtended.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UtopiaLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class UtopiaConnectionExampleExtended : MonoBehaviour
 {
@@ -17,6 +19,10 @@ public class UtopiaConnectionExampleExtended : MonoBehaviour
 
 	public GameObject PanelConnection;
 	public GameObject PanelPlayground;
+	public RawImage userAvatarImage;
+	
+	public Text UserNameText;
+	public Text UserMoodText;
 
 	protected UtopiaLib.Client client;
 	protected string endpoint;
@@ -56,5 +62,36 @@ public class UtopiaConnectionExampleExtended : MonoBehaviour
 		
 		PanelConnection.SetActive(!is_connected);
 		PanelPlayground.SetActive(is_connected);
+		loadAccountData();
     }
+	
+	public JObject getClientContact() {
+		return client.getOwnContact ();
+	}
+	
+	public string getClientPubkey() {
+		JObject account_data = client.getOwnContact ();
+		//Debug.Log(account_data.ToString());
+		return account_data ["pk"].ToString ();
+	}
+	
+	public void loadAccountData() {
+		//get client pubkey
+		string user_pubkey = getClientPubkey();
+		//load avatar
+		userAvatarImage.texture = getAccountAvatar(user_pubkey);
+		//load username
+		JObject contact_data = getClientContact();
+		
+		UserNameText.text = contact_data ["nick"].ToString ();
+		UserMoodText.text = contact_data ["moodMessage"].ToString ();
+	}
+	
+	public Texture2D getAccountAvatar(string account_pubkey) {
+		string avatar_base64 = client.getContactAvatar (account_pubkey);
+		byte[] avatar_bytes = System.Convert.FromBase64String(avatar_base64);
+		Texture2D avatar_texture = new Texture2D(1, 1);
+		avatar_texture.LoadImage( avatar_bytes );
+		return avatar_texture;
+	}
 }
